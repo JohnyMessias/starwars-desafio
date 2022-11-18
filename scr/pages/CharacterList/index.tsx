faf1a49a4497a80e002136ea3a4c7d2989fa545f
Johny Messias <3jmessias@gmail.com>
2022-11-18 14:44:45 -0300

    Ajustes

--- scr/pages/CharacterList/index.tsx
+++ scr/pages/CharacterList/index.tsx
@@ -1,69 +1,45 @@
 import React, { useState } from "react";
-import { View, Text, TouchableOpacity, Alert, StyleSheet, ScrollView, Button } from "react-native";
+import { View, Text, TouchableOpacity, Alert, StyleSheet, FlatList } from "react-native";
 import { useNavigation } from "@react-navigation/native"
 import { propsStack } from "../../routes/Stack/Models";
 
-const pernonagensIniciais = [
-  { id: 1, nome: "Personagem 01"},
-  { id: 4, nome: "Personagem 04"},
-  { id: 7, nome: "Personagem 07"},
-];
+import Header from "../../Components/header/Header";
+import PeopleList from "../../Components/header/PeopleList";
+import axios from 'axios'
 
-const CharacterList = () => {
-    const navigation = useNavigation<propsStack>() 
 
-    const [allPersons, setAllPersons ] = useState(null)
-    const [ personagemEscolhido, setPersonagemEscolhido ] = useState(null);
-    
-    const getPersonData = (idPerson) => {
-      const endpoint = `https://swapi.dev/api/people/${idPerson}/`;
-  
-      fetch(endpoint)
-        .then(response => response.json())
-          .then( result => {
-            const person = {
-              nome: result.name,
-              planetaOrigem: result.homeworld,
-              genero: result.gender,
-              anoNascimento: result.birth_year,
-              peso: result.mass,
-            };
-  
-            setPersonagemEscolhido(person);
-          })
-          .catch(() => {
-            Alert.alert('Erro', 'Não foi possível carregar os dados do personagem');
-          });
-  
+
+export default class CharacterList extends React.Component {
+    constructor(props) {
+      super(props);
+
+      this.state = {
+        peoples: []
+      };
+
     }
 
-    function getAllPersons(){
-      const endpoint = `https://swapi.dev/api/people/`;
-  
-      fetch(endpoint)
-        .then(response => response.json())
-          .then( result => {
-            const persons = result.results.map(person => person.name)
-            console.log("##Persons",persons)
-            return persons
-          })
-          .catch(() => {
-            Alert.alert('Erro', 'Não foi possível carregar a lista de personagens');
-          });
-  
+    componentDidMount(): void {
+      axios
+      .get('https://swapi.dev/api/people/')
+      .then(response => {
+        const { results } = response.data;
+        this.setState({
+          peoples: results
+        });
+      })
     }
 
-    return (
-      
-      <View style={styles.container}>
-        <View style={styles.topo}>
-          <Text style={styles.topoTitulo}>Lista de Personagens</Text>
+
+    render(){
+      return (
+        <View>
+          <Header title="Personagens" />
+          <PeopleList peoples={this.state.peoples} />
         </View>
-    </View>
-    )
+      )
 }
-
-export default CharacterList
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -119,8 +95,4 @@ const styles = StyleSheet.create({
     width: 150,
     height: 150,
   }
-});
-
-function render() {
-    throw new Error("Function not implemented.");
-  }
+})
--- scr/routes/Stack/index.tsx
+++ scr/routes/Stack/index.tsx
@@ -1,6 +1,5 @@
 import React from "react"
 import { createNativeStackNavigator } from "@react-navigation/native-stack"
-
 import Home from "../../pages/Home"
 import Login from "../../pages/Login"
 import CharacterDetails from "../../pages/CharacterDetails"
@@ -9,17 +8,26 @@ import PlanetDetails from "../../pages/PlanetDetails"
 import PlanetList from "../../pages/PlanetsList"
 import { propsNavigationStack } from "./Models"
 
-const {Navigator, Screen } = createNativeStackNavigator()
+const Stack = createNativeStackNavigator()
 
-export default function() {
+export default function Routes() {
     return(
-        <Navigator initialRouteName="Login" screenOptions={{headerShown: false}}>
-            <Screen name="Login" component={Login} />
-            <Screen name="Home" component={Home} />
-            <Screen name="CharacterDetails" component={CharacterDetails} />
-            <Screen name="CharacterList" component={CharacterList} />
-            <Screen name="PlanetDetails" component={PlanetDetails} />
-            <Screen name="PlanetList" component={PlanetList} />
-        </Navigator>
+        <Stack.Navigator initialRouteName="Home">
+            <Stack.Screen 
+               name="Home" 
+               component={Home} />
+            <Stack.Screen 
+               name="CharacterDetails" 
+               component={CharacterDetails} />
+            <Stack.Screen 
+               name="CharacterList" 
+               component={CharacterList} />
+            <Stack.Screen 
+               name="PlanetDetails" 
+               component={PlanetDetails} />
+            <Stack.Screen 
+               name="PlanetList" 
+               component={PlanetList} />
+        </Stack.Navigator>
     )
 }
